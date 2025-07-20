e461cee8eea4f8d067d69c25999ebdc7334c14c9
Satrio Ponco Sushadi <66540@hq.bni.co.id>
2025-07-21 00:35:30 +0700

    add csp

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sequelize", "pg", "pg-hstore"],
+  poweredByHeader: false,
 };
 
 export default nextConfig;
