f17d958acc8466901a89712ea280bc1d9d79710a
Satrio Ponco Sushadi <66540@hq.bni.co.id>
2025-07-21 00:11:24 +0700

    try so hard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sequelize", "pg", "pg-hstore"],
+  webpack: (config) => {
+    config.externals = [
+      ...(typeof config.externals === "function"
+        ? [config.externals]
+        : config.externals || []),
+      {
+        sequelize: "commonjs sequelize",
+        pg: "commonjs pg",
+        "pg-hstore": "commonjs pg-hstore",
+      },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
