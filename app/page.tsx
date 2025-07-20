6ef70d8d52cd2779787e20a90a46f25f04e2e388
Satrio Ponco Sushadi <66540@hq.bni.co.id>
2025-07-21 00:07:02 +0700

    try so hard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export const dynamic = "force-dynamic";
 
+("use server");
+
 import HomepageClient from "@/clients/client";
 import LandingPage from "@/models/LandingPage";
 import SystemParameter from "@/models/SystemParameter";
--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sequelize", "pg", "pg-hstore"],
-  webpack: (config) => {
-    // Ensure that the 'sequelize' package is not bundled by Next.js
-    config.externals = {
-      ...config.externals,
-      sequelize: "commonjs sequelize",
-      pg: "commonjs pg",
-      "pg-hstore": "commonjs pg-hstore",
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
